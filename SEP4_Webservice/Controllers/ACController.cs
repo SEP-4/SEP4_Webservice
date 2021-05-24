8f2ecf5fe24220463d1a10e8943440268d5507aa
Kevin Pongó <293123@VIAUC.DK>
2021-05-24 11:47:43 +0200

    fix controllers

---
+++ SEP4_Webservice/Controllers/ACController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SEP4_Webservice.Data;
+using SEP4_Webservice.Model;
+
+namespace SEP4_Webservice.Controllers
+{
+    [Produces("application/json")]
+    [ApiController]
+    [Route("[controller]")]
+    public class ACController : ControllerBase
+    {
+        private IDeviceService DeviceService;
+        public ACController(IDeviceService deviceService)
+        {
+            DeviceService = deviceService;
+        }
+
+        [HttpGet]
+        [Route("turnOff/{Gym_ID:int}")]
+        public async Task<ActionResult> TurnOffAC([FromRoute] int Gym_ID)
+        {
+            try
+            {
+                await DeviceService.turnOffAC(Gym_ID);
+                return Ok("AC is turned off!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("turnOn/{Gym_ID:int}")]
+        public async Task<ActionResult> TurnOnAC([FromRoute] int Gym_ID)
+        {
+            try
+            {
+                 await DeviceService.turnOnAC(Gym_ID);
+                return Ok("AC is turned on!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{Gym_ID:int}")]
+        public async Task<ActionResult<AC>> GetACByGymID([FromRoute] int Gym_ID)
+        {
+            try
+            {
+                AC ac = await DeviceService.GetACByGymID(Gym_ID);
+                return Ok(ac);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
---
+++ SEP4_Webservice/Controllers/HumidifierController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SEP4_Webservice.Data;
+using SEP4_Webservice.Model;
+
+namespace SEP4_Webservice.Controllers
+{
+    [Produces("application/json")]
+    [ApiController]
+    [Route("[controller]")]
+    public class HumidifierController : ControllerBase
+    {
+        private IDeviceService DeviceService;
+        public HumidifierController(IDeviceService deviceService)
+        {
+            DeviceService = deviceService;
+        }
+
+        [HttpGet]
+        [Route("turnOff/{Gym_ID:int}")]
+        public async Task<ActionResult> TurnOffHumidifier([FromRoute] int Gym_ID)
+        {
+            try
+            {
+                await DeviceService.turnOffHumidifier(Gym_ID);
+                return Ok("Humidifier is turned off!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("turnOn/{Gym_ID:int}")]
+        public async Task<ActionResult> TurnOnHumidifier([FromRoute] int Gym_ID)
+        {
+            try
+            {
+                await DeviceService.turnOnHumidifier(Gym_ID);
+                return Ok("Humidifier is turned on!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{Gym_ID:int}")]
+        public async Task<ActionResult<Humidifier>> GetHumidifierByGymID([FromRoute] int Gym_ID)
+        {
+            try
+            {
+                Humidifier humidifier = await DeviceService.GetHumidifierByGymID(Gym_ID);
+                return Ok(humidifier);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
